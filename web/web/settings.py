8853e684a93ab73cd6f41fcf1ece5063b0e46ffe
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2023-12-19 15:55:35 +0300

    try more commit

--- web/web/settings.py
+++ web/web/settings.py
@@ -25,12 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&seqbegnc0z*$ror2r6qod$@j7r@*v_!r&@vfv29dh17m0$lx%'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['bro-pro-game-dev.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
