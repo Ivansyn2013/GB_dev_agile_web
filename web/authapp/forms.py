459120a4635f3f6272119c19f91c2b65727b2a8c
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2023-12-19 14:30:44 +0300

    try more commit

--- web/authapp/forms.py
+++ web/authapp/forms.py
@@ -30,11 +30,11 @@ class CustomUserCreationForm(UserCreationForm):
     def save(self, commit=True):
         user = super().save(commit=False)
         user.set_password(self.cleaned_data['password1'])
-        user.is_active = False
-        user.is_activated = False
+        user.is_active = True
+        user.is_activated = True
         if commit:
             user.save()
-        user_registered.send(CustomUserCreationForm, instance=user)
+        # user_registered.send(CustomUserCreationForm, instance=user)
         return user
 
     class Meta:
