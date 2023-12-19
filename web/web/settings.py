be9fb04dd8c45c2fc2474ffd3d8ff5d8ffb97eeb
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2023-12-19 16:14:38 +0300

    try more commit

--- web/web/settings.py
+++ web/web/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     # 'channels',
     'authapp',
     'bootstrap4',
+    'web',
 
 ]
 
@@ -136,6 +137,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
+
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
