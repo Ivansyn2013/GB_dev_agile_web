2e76585c963ca708c09ed084a11a00c054a0cab6
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2023-12-20 15:03:39 +0300

    build.sh add

--- web/web/settings.py
+++ web/web/settings.py
@@ -138,9 +138,10 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [(
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)]
+)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
