38f71f4d08c2eb137804f438a50a4da714a127fb
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2024-02-21 13:38:37 +0300

    try more depl_20

--- web/web/urls.py
+++ web/web/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 from authapp.views import home, RegisterUser, LoginUser, profile_user_view, top_players, logout_user, \
     ProfileUpdateView, RegisterDoneView, user_activate, login_github, login_github_callback, login_vk, login_vk_callback
-
 
 
 urlpatterns = [
@@ -42,8 +42,9 @@ urlpatterns = [
     path('login/github/callback/', login_github_callback, name='login_github_callback'),
     path('login/vk/', login_vk, name='login_vk'),
     path('login/vk/callback/', login_vk_callback, name='login_vk_callback'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
