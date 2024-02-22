b8094215695a594770e32382eebc73b347bd7670
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2024-02-22 13:22:21 +0300

    try more depl_28

--- web/start.sh
+++ web/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # exit on error
 set -o errexit
-daphne web.asgi:application --port 8001 && gunicorn web.asgi:application -k uvicorn.workers.UvicornWorker
+gunicorn web.asgi:application -k uvicorn.workers.UvicornWorker && daphne -b 0.0.0.0 web.asgi:application --port 8001
 #gunicorn web.asgi:application -k uvicorn.workers.UvicornWorker
