14f83a246b0154071915139dcca0c3665d7fd220
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2024-02-22 12:36:03 +0300

    try more depl_27

--- web/start.sh
+++ web/start.sh
@@ -2,4 +2,4 @@
 # exit on error
 set -o errexit
 #daphne web.asgi:application --port 8001 && python -m gunicorn web.asgi:application -k uvicorn.workers.UvicornWorker
-python -m gunicorn web.asgi:application -k uvicorn.workers.UvicornWorker
+gunicorn web.asgi:application -k uvicorn.workers.UvicornWorker
