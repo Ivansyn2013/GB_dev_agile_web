415e345b355b10a98fa1c57f4f03163a21f6a6a4
KonstantinSuchkov <konstantinsuchkov@yandex.ru>
2024-02-22 12:30:31 +0300

    try more depl_27

--- web/start.sh
+++ web/start.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # exit on error
 set -o errexit
 #daphne web.asgi:application --port 8001 && python -m gunicorn web.asgi:application -k uvicorn.workers.UvicornWorker
